1bdcc106a3fd3d071df90aa8a472d3db70e1aa04
hayao <shun819.mail@gmail.com>
2026-03-03 15:53:31 +0900

    chore: add eslint-plugin-simple-import-sort

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,17 @@
 const reactNativeConfig = require("@react-native/eslint-config/flat");
+const simpleImportSort = require("eslint-plugin-simple-import-sort");
 
 module.exports = [
   ...reactNativeConfig,
   {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
     rules: {
       "ft-flow/define-flow-type": "off",
       "ft-flow/use-flow-type": "off",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   },
 ];
